9a2ebf5f3fe816da58740e4ec949c5198607e36b
Yoav Rubin <yoav.rubin@post.runi.ac.il>
2025-12-27 20:02:58 +0200

    v01

--- Koch.java
+++ Koch.java
@@ -27,8 +27,23 @@ public class Koch {
 	/** Gets n, x1, y1, x2, y2,
      *  and draws a Koch curve of depth n from (x1,y1) to (x2,y2). */
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
-		StdDraw.line(x1, y1, x2, y2);
-		//// Write the rest of your code below.
+		if(n==0){
+			StdDraw.line(x1, y1, x2, y2);
+            return;}
+			//לחלק ל 3 חלקים שווים
+			double p1x = x1+ (x2-x1)/3.0;
+			double p1y = y1+ (y2-y1)/3.0;
+			double p3x = x1+ 2*(x2-x1)/3.0;
+			double p3y = y1+ 2*(y2-y1)/3.0;
+			//קצה המשולש
+        double sqrt3 = Math.sqrt(3);
+        double p2x = (p1x+ p3x) / 2.0 + (sqrt3 / 2.0) * (p1y-p3y);
+        double p2y = (p1y+ p3y) / 2.0 + (sqrt3 / 2.0) * (p3x-p1x);
+		//קריאה לרקורסיה עבור הקווים הרלוונטיים
+		curve(n-1,x1,y1,p1x,p1y);
+		curve(n-1,p1x,p1y,p2x,p2y);
+		curve(n-1,p2x,p2y,p3x,p3y);
+		curve(n-1,p3x,p3y,x2,y2);
 	}
 
     /** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
@@ -36,7 +51,12 @@ public class Koch {
 		// A little tweak that makes the drawing look better
 		StdDraw.setYscale(0, 1.1);
 		StdDraw.setXscale(0, 1.1);
-		// Draws a Koch snowflake of depth n
-		//// Write the rest of your code below.
-	}
+        double v1x = 0.2, v1y = 0.35; // Bottom left
+        double v2x = 0.8, v2y = 0.35; // Bottom right
+        double h = Math.sqrt(3) / 2 * (v2x - v1x);
+        double v3x = 0.5, v3y = v1y + h; 
+        curve(n, v1x, v1y, v3x, v3y); // Left side
+        curve(n, v3x, v3y, v2x, v2y); // Right side
+        curve(n, v2x, v2y, v1x, v1y); // Bottom side
+    }
 }
--- Sierpinski.java
+++ Sierpinski.java
@@ -7,12 +7,29 @@ public class Sierpinski {
 	
 	// Draws a Sierpinski triangle of depth n on the standard canvass.
 	public static void sierpinski (int n) {
-		//// Replace this comment with your code
+    double[] x = { 0, 1, 0.5 };
+    double[] y = { 0, 0, 0.866 };
+    StdDraw.setPenColor(StdDraw.BLACK);
+    StdDraw.filledPolygon(x, y);
+    sierpinski(n, x[0], x[1], x[2], y[0], y[1], y[2]);
 	}
 	
 	// Does the actual drawing, recursively.
 	private static void sierpinski(int n, double x1, double x2, double x3,
 		                                 double y1, double y2, double y3) {
-		//// Replace this comment with your code
-	}
+		if (n == 0) return;
+        double x12 = (x1 + x2)/2;
+        double y12 = (y1 + y2)/2;
+		double x23 = (x2 + x3)/2;
+        double y23 = (y2 + y3)/2;
+		double x13 = (x3 + x1)/2;
+        double y13 = (y3 + y1)/2;
+		StdDraw.setPenColor(StdDraw.WHITE);
+        double[] xMid = { x12, x23, x13 };
+        double[] yMid = { y12, y23, y13 };
+        StdDraw.filledPolygon(xMid, yMid);
+		sierpinski(n-1,x3,x13,x23,y3,y13,y23);
+		sierpinski(n-1,x2,x12,x23,y2,y12,y23);
+		sierpinski(n-1,x1,x12,x13,y1,y12,y13);
+}
 }
